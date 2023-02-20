4069e46dd125a18b5a7a6280a8da04ece9d4b67e
PettoDavida <maximilian.widman@gmail.com>
2023-02-20 16:35:39 +0100

    New content

--- src/api/partner/routes/partner.js
+++ src/api/partner/routes/partner.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * test router
+ * partner router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::test.test');
+module.exports = createCoreRouter('api::partner.partner');
