d3e90e38f73f1f81b30790a01a23edfc6ff0a443
lazar93kg <lazar93kg@gmail.com>
2023-04-17 16:20:26 +0200

    test3

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,18 @@
-const path = require("path");
-
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "bank"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "0000"),
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+const parse = require('pg-connection-string').parse;
+    const config = parse(process.env.DATABASE_URL);
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false
+          },
+        },
+        debug: false,
       },
-    },
-    debug: false,
-  },
-});
+    });
