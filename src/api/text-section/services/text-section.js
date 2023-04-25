79b0eaed6ce24434a151d915365ec2519f5becd1
lazar93kg <lazar93kg@gmail.com>
2023-04-25 14:46:44 +0200

    TextSection

---
+++ src/api/text-section/services/text-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * text-section service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::text-section.text-section');
